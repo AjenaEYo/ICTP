99dcb972cb0018fa693da4331da861c83a9c1bf2
AjenaEYO <ruinandgod@gmail.com>
2021-05-16 23:35:40 +0900

    dummy2

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ import cv2 as cv
 import numpy as np
 import os
 from time import time
-from googletrans import Translator
 import easyocr
-
+from google_trans_new import google_translator
 from module.WindowCapture import WindowCapture
 from module.Vision import Vision
+
+from PIL import ImageFont, ImageDraw, Image
+
 
 # Change the working directory to the folder this script is in.
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def draw_box(image, bounds,color = 'yellow',width=2):
-    for bound in bounds:
+def draw_text(image, bounds,color = 'yellow',width=2):
+    img = Image.fromarray(image)
+    draw = ImageDraw.Draw(img)
+    font=ImageFont.truetype("fonts/gulim.ttc",20)
+    for bound  in bounds:
         (top_left, top_right, bottom_right, bottom_left)=bound[0]
+        text = bound[1];
         top_left = (int(top_left[0]), int(top_left[1]))
         bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+        draw.text(top_left,text,font=font,fill=(0,0,0))
+        #cv.rectangle(img=image, pt1=top_left, pt2=bottom_right, color=(255, 0, 0), thickness=10)
+
+def draw_box(image, bounds,color = 'yellow',width=2):
+    for bound  in bounds:
+        (top_left, top_right, bottom_right, bottom_left)=bound[0]
+        text = bound[1];
+        top_left = (int(top_left[0]), int(top_left[1]))
+        bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+
         cv.rectangle(img=image, pt1=top_left, pt2=bottom_right, color=(255, 0, 0), thickness=10)
+        #cv.putText(image, text, (top_left[0], bottom_left[1] - 10),cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         
 
     
@@ -32,7 +49,10 @@ wincap = WindowCapture()
 vision_gunsnbottle = Vision('gunsnbottle.jpg')
 '''
 reader = easyocr.Reader(['en']);
-translator = Translator();
+
+translator = google_translator()
+#translate_text = translator.translate('I am GROOT',lang_src='en',lang_tgt='ko')
+#print(translate_text)
 loop_time = time()
 while(True):
 
@@ -43,8 +63,16 @@ while(True):
     #points = vision_limestone.find(screenshot, 0.5, 'rectangles')
     #points = vision_gunsnbottle.find(screenshot, 0.7, 'points')
     bounds = reader.readtext(screenshot, add_margin=0.55, width_ths=0.7, link_threshold=0.8, decoder='beamsearch', blocklist='=-')  #Reading with bounds
+    bounds_ko = []
     #print(bounds)
-    draw_box(screenshot,bounds)
+    for bound  in bounds:
+        text = bound[1]
+        text_ko = translator.translate(text,lang_tgt='ko',lang_src='en')
+        y = list(bound)
+        y[1] = text_ko
+        bounds_ko.append(tuple(y))
+ 
+    draw_box(screenshot,bounds_ko)
     cv.imshow('Computer Vision', screenshot)
     # debug the loop rate
     print('FPS {}'.format(1 / (time() - loop_time)))
